58deb3ce21b96dac51a10215dfa2ca01f3ddd284
ilkiz <ilkiz@ilkiz>
2022-06-16 17:55:02 +0300

    .

---
+++ src/com/Ders004/odev2.java
@@ -0,0 +1,17 @@
+package com.Ders004;
+
+public class odev2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 1- (Kolay) 5 ile 7 nin toplamý nedir? 2- (Kolay) iki sayým var biri 9 diðer 2 bunlarýn çarpýmý nedir? 3-(Zor) iki
+		 * sayý 124 ile 458 sayýlarýnýn çarpýmý nedir?
+		 * 
+		 * bunlar birer string ifadedir. bunlarý kullanýcýdan alýcaz. kolay olanlarda sadece rakamlar olacak (0-9 arasý) temel
+		 * iþlemlerimiz (topla, fark, çarp, böl) iþlemin sonucunu yazdýran program.
+		 * 
+		 */
+	}
+
+}
